38cbad111424c309b7c9fd6c3a4bcc91d86e3c0d
Varsha Devidas Ghule <varshaghule41@gmail.com>
2025-05-29 11:11:41 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
 const App = () => {
-  const url = "http://localhost:4000";
+  const url = "https://cravings-backend-b7kq.onrender.com";
   const [user, setUser] = useState(null);
   const [showLogin, setShowLogin] = useState(true); 
   const handleLogin = (userData) => {
