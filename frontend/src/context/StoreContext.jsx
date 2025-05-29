09c56ed41da9a3507e223ce8432abaf7f2eb57f2
Varsha Devidas Ghule <varshaghule41@gmail.com>
2025-05-29 11:09:00 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -4,7 +4,7 @@ import { createContext, useEffect, useState } from "react";
 export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
     const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:4000";
+    const url = "https://cravings-backend-b7kq.onrender.com";
     const [token, setToken] = useState("");
     const [food_list, setFoodList] = useState([])
     const addToCart = async (itemId) => {
